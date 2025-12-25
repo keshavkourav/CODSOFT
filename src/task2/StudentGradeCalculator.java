8244956da8f4352616089799265605b48504e4ea
keshavkourav <keshavkourav107@gmail.com>
2025-12-25 17:18:26 +0530

    updated Task 2 - Student Grade Calculator

--- src/task2/StudentGradeCalculator.java
+++ src/task2/StudentGradeCalculator.java
@@ -1,39 +1,27 @@
-package task2;
 
+package task2;
 import java.util.Scanner;
 
 public class StudentGradeCalculator {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
-        System.out.print("Enter number of subjects: ");
-        int subjects = sc.nextInt();
-
-        int total = 0;
-
-        for (int i = 1; i <= subjects; i++) {
-            System.out.print("Enter marks of subject " + i + ": ");
-            int marks = sc.nextInt();
-            total = total + marks;
+        System.out.print("How many subjects? ");
+        int sub = sc.nextInt();
+        int tot = 0;
+        for(int i=0;i<sub;i++){
+            System.out.print("Marks for subject "+(i+1)+": ");
+            tot += sc.nextInt();
         }
-
-        double average = (double) total / subjects;
-        String grade;
-
-        if (average >= 90) {
-            grade = "A";
-        } else if (average >= 80) {
-            grade = "B";
-        } else if (average >= 70) {
-            grade = "C";
-        } else if (average >= 60) {
-            grade = "D";
-        } else {
-            grade = "F";
-        }
-
-        System.out.println("Total Marks: " + total);
-        System.out.println("Average Percentage: " + average);
-        System.out.println("Grade: " + grade);
+        double avg = tot/(double)sub;
+        String grd;
+        if(avg>=90) grd="A";
+        else if(avg>=80) grd="B";
+        else if(avg>=70) grd="C";
+        else if(avg>=60) grd="D";
+        else grd="F";
+        System.out.println("Total: "+tot);
+        System.out.println("Average: "+avg);
+        System.out.println("Grade: "+grd);
     }
 }
+
